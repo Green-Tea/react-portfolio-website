d7c469b091291a5759463d70b37785a73df24e79
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-08-24 16:59:31 +0700

    updated socials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import { FaGithubSquare, FaInstagramSquare } from 'react-icons/fa'
 function HeaderSocials() {
     return (
         <div className='header__socials'>
-            <a href='https://github.com/Green-Tea' target='__blank'><FaGithubSquare size={23} /></a>
             <a href='discordapp.com/users/175950770775130112' target='__blank'><BsDiscord size={23} /></a>
+            <a href='https://github.com/Green-Tea' target='__blank'><FaGithubSquare size={23} /></a>
             <a href='https://instagram.com/lorenzzo.c' target='__blank'><FaInstagramSquare size={23} /></a>
         </div>
     )
