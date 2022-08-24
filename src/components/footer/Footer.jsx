0cc478406559bb0e2670885e95e0950c2fadda12
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-08-24 18:12:32 +0700

    added footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import './footer.css'
+import { BsDiscord } from 'react-icons/bs'
+import { FaGithubSquare, FaInstagramSquare } from 'react-icons/fa'
 
 function Footer() {
     return (
-        <div>
+        <footer>
+            <ul className='permalinks'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#experience'>Experience</a></li>
+                <li><a href='#portfolio'>Portfolio</a></li>
+            </ul>
+
+            <div className='footer__socials'>
+                <a href='discordapp.com/users/175950770775130112' target='__blank'><BsDiscord size={23} /></a>
+                <a href='https://github.com/Green-Tea' target='__blank'><FaGithubSquare size={23} /></a>
+                <a href='https://instagram.com/lorenzzo.c' target='__blank'><FaInstagramSquare size={23} /></a>
+            </div>
+
             <small>
                 <a href='https://www.freepik.com/vectors/connect-background'>Connect background vector created by rawpixel.com - www.freepik.com</a>
             </small>
-        </div>
+        </footer>
 
     )
 }
