9a4e5e8a1e6b04850bd6d32a64f7aa88f6cff57c
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-08-25 13:32:30 +0700

    remove unused component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     return (
         <footer>
             <ul className='permalinks'>
-                <li><a href='#'>Home</a></li>
+                <li><a href='#home'>Home</a></li>
                 <li><a href='#about'>About</a></li>
                 <li><a href='#experience'>Experience</a></li>
                 <li><a href='#portfolio'>Portfolio</a></li>
