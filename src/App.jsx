00113215fd8dc70149060e996981e8dd0a755b6b
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-08-25 13:30:01 +0700

    Revert "Hide navbar when scroll to the bottom"
    This reverts commit 92c614ccff18d9959f2f22f9d4db522e587bb9b6.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { HideOn } from 'react-hide-on-scroll'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
+// import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
@@ -12,12 +12,11 @@ import Footer from './components/footer/Footer'
 const App = () => {
     return (
         <>
-            <HideOn className='hide' atHeight height={3500}>
-                <Nav />
-            </HideOn>
             <Header />
+            <Nav />
             <About />
             <Experience />
+            {/* <Services /> */}
             <Portfolio />
             <Contact />
             <Footer />
